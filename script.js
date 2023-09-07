78780761bac0457e7b4bff38b6b818639ddda35a
ketanpandagre <ketanpandagre11@gmail.com>
2023-09-07 21:23:44 +0530

    changes

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ icon.onclick = function() {
         icon.src="./assets/sun.svg";
     }
 };
+//changing text
+(function(){
+    var words=[
+        "Mechanics",
+        "Plumbers",
+        "Cleaners",
+        "Electricians",
+        "Labours"
+    ],
+    i=0;
+    setInterval(function(){
+        $('#words').fadeOut(function(){
+            $(this).html(words[(i=(i+1)%words.length)]).fadeIn();
+        });
+    },2000)
+})();
